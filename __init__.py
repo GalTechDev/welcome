0b03c2574e1f6473d6fc80fa7db93b47ecd64e9f
GalTechDev <poussigalitv@gmail.com>
2023-08-02 15:53:18 +1100

    re-organisation

--- __init__.py
+++ __init__.py
@@ -1,80 +1,110 @@
 from understar.system import lib
 import discord
+from discord import Member
 from PIL import Image, ImageDraw, ImageOps, ImageFont
 Lib = lib.App()
 
-welcome_message = """
-Bienvenue {}, dans le serveur
+banner_folder = "guilds"
+avatar_folder = "users"
 
-Si tu as la moindre question, n'hésite pas a demander de l'aide
-"""
-
-
+guilds_config = {} # guild_id:{"type":"", "data":{"Embed":{}, "Banner":{}}}
 
 # -------------------------------- Event --------------------------------------
 
 @Lib.event.event()
 async def on_member_join(member: discord.Member):
-    channel = member.guild.system_channel
-    embed = discord.Embed(title="Bienvenu!", description=welcome_message.format(member.mention), color=discord.Color.yellow())
-    embed.set_thumbnail(url=member.guild.icon.url)
-    await channel.send(embed=embed)
-    #await send_banner(member)
+    msg_class = types.get("Embed")
+    if msg_class:
+        await msg_class.join_msg(member)
+        
 
 @Lib.event.event()
 async def on_member_remove(member: discord.Member):
-
-    channel = member.guild.system_channel
-    embed = discord.Embed(title=f"Oh non! {member.name} nous a quitté!", color=discord.Color.yellow())
-    await channel.send(embed=embed)
+    msg_class = types.get("Embed")
+    if msg_class:
+        await msg_class.leave_msg(member)
 
 # --------------------------------- Type --------------------------------------
+class msg:
+    @property
+    async def join_msg(member: Member): None
+        
+    @property
+    async def leave_msg(member: Member): None
 
-async def send_banner(member: discord.Member):
-    # Ouvre l'image template.png
-    image = Image.open(Lib.save.get_full_path(name='template.png'))
-    draw = ImageDraw.Draw(image)
+class Embed_msg(msg):
+    welcome_message =   """
+                        Bienvenue {}, dans le serveur
+
+                        Si tu as la moindre question, n'hésite pas a demander de l'aide
+                        """
+                        
+    async def join_msg(member: Member):
+        channel = member.guild.system_channel
+        embed = discord.Embed(title="Bienvenu!", description=Embed_msg.welcome_message.format(member.mention), color=discord.Color.yellow())
+        embed.set_thumbnail(url=member.guild.icon.url)
+        await channel.send(embed=embed)
+        
+    async def leave_msg(member: Member):
+        channel = member.guild.system_channel
+        embed = discord.Embed(title=f"Oh non! {member.name} nous a quitté!", color=discord.Color.yellow())
+        await channel.send(embed=embed)
+
+class Banner_msg(msg):
     
-    # Télécharge l'avatar de l'utilisateur qui a rejoint le serveur
-    avatar_data = await member.avatar.read()
-    Lib.save.write(name=f"{member.id}_avatar.png", path="avatar", data=avatar_data, binary_mode=True)
+    async def gen_image_path(member: Member):
+        # Ouvre l'image template.png
+        image = Image.open(Lib.save.get_full_path(name='template.png'))
+        draw = ImageDraw.Draw(image)
+        
+        # Télécharge l'avatar de l'utilisateur qui a rejoint le serveur
+        avatar_data = await member.avatar.read()
+        Lib.save.write(name=f"{member.id}_avatar.png", path="avatar", data=avatar_data, binary_mode=True)
 
-    # Ouvre l'avatar téléchargé
-    avatar = Image.open(Lib.save.get_full_path(name=f"{member.id}_avatar.png", path="avatar")).convert('RGBA')
+        # Ouvre l'avatar téléchargé
+        avatar = Image.open(Lib.save.get_full_path(name=f"{member.id}_avatar.png", path="avatar")).convert('RGBA')
 
-    # Redimensionne l'avatar à la taille souhaitée
-    avatar = avatar.resize((200, 200), Image.ANTIALIAS)
-    mask = Image.new('L', avatar.size, 0)
-    draw_mask = ImageDraw.Draw(mask)
-    draw_mask.ellipse((0, 0, avatar.width, avatar.height), fill=255)
+        # Redimensionne l'avatar à la taille souhaitée
+        avatar = avatar.resize((200, 200), Image.ANTIALIAS)
+        mask = Image.new('L', avatar.size, 0)
+        draw_mask = ImageDraw.Draw(mask)
+        draw_mask.ellipse((0, 0, avatar.width, avatar.height), fill=255)
 
-    # Application du masque à l'avatar
-    avatar = ImageOps.fit(avatar, mask.size, centering=(0.5, 0.5))
-    avatar.putalpha(mask)
+        # Application du masque à l'avatar
+        avatar = ImageOps.fit(avatar, mask.size, centering=(0.5, 0.5))
+        avatar.putalpha(mask)
 
-    # Calcule les coordonnées du centre de l'image
-    center_x = (image.width - avatar.width) // 2
-    center_y = (image.height - avatar.height) // 2
+        # Calcule les coordonnées du centre de l'image
+        center_x = (image.width - avatar.width) // 2
+        center_y = (image.height - avatar.height) // 2
 
-    # Ajoute l'avatar au centre de l'image
-    image.paste(avatar, (center_x, center_y), avatar)
+        # Ajoute l'avatar au centre de l'image
+        image.paste(avatar, (center_x, center_y), avatar)
+        
+        """# Ajout du nom de l'utilisateur en bas de l'image
+        font = ImageFont.truetype('arial.ttf', 24)
+        text_width, text_height = draw.textsize(member.name, font=font)
+        text_x = (image.width - text_width) // 2
+        text_y = image.height - text_height - 10
+        draw.text((text_x, text_y), member.name, font=font, fill=(255, 255, 255))"""
+
+        # Enregistre l'image modifiée avec le nom de l'utilisateur
+        path = Lib.save.get_full_path(name=f'{member.id}_banner.png', path=avatar_folder)
+        image.save(path)
+        
+        return path
     
-    # Ajout du nom de l'utilisateur en bas de l'image
-    font = ImageFont.truetype('arial.ttf', 24)
-    text_width, text_height = draw.textsize(member.name, font=font)
-    text_x = (image.width - text_width) // 2
-    text_y = image.height - text_height - 10
-    draw.text((text_x, text_y), member.name, font=font, fill=(255, 255, 255))
+    async def join_msg(member: Member):
+        # Envoie l'image sur le canal de bienvenue
+        channel = member.guild.system_channel
+        if not channel:
+            return
+                
+        await channel.send(file=discord.File(await Banner_msg.gen_image_path(member)))
 
-    # Enregistre l'image modifiée avec le nom de l'utilisateur
-    image.save(f'{member.id}_banner.png')
 
-    # Envoie l'image sur le canal de bienvenue
-    channel = member.guild.system_channel
-    if not channel:
-        return
-            
-    await channel.send(file=discord.File(f'{member.id}_banner.png'))
+types = {"Embed":Embed_msg, "Banner": Banner_msg}
+    
 
 # --------------------------------- Modal ------------------------------------- 
 
